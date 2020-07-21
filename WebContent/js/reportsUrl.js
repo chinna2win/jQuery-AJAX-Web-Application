fc040dd0510708e2839fc1bc2447ef40692e4c24
Chinnasamy Thangraj (KUL) <kul_chinnasamy@incontrol-tech.local>
2020-07-21 23:52:39 +0800

    fixes

--- WebContent/js/reportsUrl.js
+++
@@ -1,9 +0,0 @@
-function reportsUrl() {
-	document.getElementById("IndividualProductivity").href = "/IMPReports/BirtReport/IndividualProductivity.jsp";
-	document.getElementById("OverallPercentageAction").href = "/IMPReports/BirtReport/OverallPercentageAction.jsp";
-	document.getElementById("NumberOfOccByDay").href = "/IMPReports/BirtReport/NumberOfOccByDay.jsp";
-	document.getElementById("NumberOfOccByTime").href = "/IMPReports/BirtReport/NumberOfOccByTime.jsp";
-	document.getElementById("NumberOfOccByWeek").href = "/IMPReports/BirtReport/NumberOfOccByWeek.jsp";
-	document.getElementById("TotalOccVersesTime").href = "/IMPReports/BirtReport/TotalOccVersesTime.jsp";
-	document.getElementById("SummaryByProject").href = "/IMPReports/BirtReport/SummaryByProject.jsp";	
-}
